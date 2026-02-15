49ffff39ae442bdb4f49eff063b3db104818bc29
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-15 22:37:42 +0800

    alvistack/1.19.5
        git clean -xdf
        tar zcvf ../python-numpy_1.19.5.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-numpy.spec ../python-numpy_1.19.5-1.spec
        cp ../python*-numpy*1.19.5*.{gz,xz,spec,dsc} /osc/home\:alvistack/numpy-numpy-1.19.5/
        rm -rf ../python*-numpy*1.19.5*.*
    
    See https://github.com/numpy/numpy/issues/27405
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.19.5'
+version = '1.19.5'
+full_version = '1.19.5'
+git_revision = '909c77de3b8bbdc42716134766f6ac4782bdd12b'
+release = True
+
+if not release:
+    version = full_version
